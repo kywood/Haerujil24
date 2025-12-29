5a96052e54e29f3274087299f2852fc3dfe60603
Wood <backboss@gmail.com>
2025-12-30 01:07:30 +0900

    feat  - commom-lib 분기  -  data-set 프로세스 개발 진행

--- app/serving/Controller/ServingController.py
+++ app/serving/Controller/ServingController.py
@@ -8,6 +8,9 @@ class ServingController(IController):
     def __init__(self,config: ConfigLoader):
         super().__init__()
 
+        from Service.ServingService import ServingService
+        self._service = ServingService(config)
+
         self._config = config
 
         from Dtos.S3Dto import S3ConnectDto
@@ -46,108 +49,19 @@ class ServingController(IController):
         # from Models.RestModel import PingRequest
         @app.get("/ping/{name}", response_model=PingResponse)
         def health_dir(name:str):
-            # from Config.ConfigHelper import ConfigHelper
-            # path = ConfigHelper.GetCachePath()
-            # print(f"path:{path} nm:{name}")
-
             from Models.RestModel import ResponseHeader
             return PingResponse(response=ResponseHeader(code=1,message="dd"),  name="a" , age=10)
 
 
-        # @app.post("/infer")
-        # async def infer(request: Request,file: UploadFile = File(...)):
-        #     ct = request.headers.get("content-type")
-        #     print("content-type header:", ct)
-        #
-        #     print("=== Uploaded File Info ===")
-        #     print("filename     :", file.filename)
-        #     print("content_type :", file.content_type)
-        #     content = await file.read()
-        #     file_size = len(content)
-        #     print("file_size    :", file_size, "bytes")
-        #
-        #     return {"status": "ok"}
-
         from Models.RestModel import InferResponse
         @app.post("/infer" , response_model=InferResponse)
         async def infer(request: Request,file: UploadFile = File(...)):
-            import tempfile
-            import os
-            import time
-
-            from Inference.InferenceManager import InferenceManager
-
-            # 1) 업로드 파일을 임시 파일로 저장
-            suffix = os.path.splitext(file.filename)[1].lower()
-            with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
-                tmp.write(await file.read())
-                tmp_path = tmp.name
-
-            try:
-                # 2) 추론 실행
-                mgr = InferenceManager.instance()
-
-                t0 = time.time()
-                results = mgr.infer(tmp_path)
-                elapsed_ms = (time.time() - t0) * 1000.0
-
-                # 3) 결과 파싱 (YOLO 결과 → JSON)
-                r = results[0]
-                detections = []
-                boxes = r.boxes
-
-                for i in range(len(boxes)):
-                    cls_id = int(boxes.cls[i])
-                    detections.append({
-                        "class_id": cls_id,
-                        "class_name": r.names.get(cls_id, str(cls_id)),
-                        "confidence": float(boxes.conf[i]),
-                        "bbox": list(map(float, boxes.xyxy[i])),  # [x1,y1,x2,y2]
-                    })
-
-                return InferResponse(
-                    filename=file.filename,
-                    elapsed_ms=elapsed_ms,
-                    count=len(detections),
-                    detections=detections
-                )
-
-                # return {
-                #     "filename": file.filename,
-                #     "elapsed_ms": elapsed_ms,
-                #     "count": len(detections),
-                #     "detections": detections
-                # }
-
-                # return JSONResponse({
-                #     "filename": file.filename,
-                #     "elapsed_ms": elapsed_ms,
-                #     "count": len(detections),
-                #     "detections": detections
-                # })
-
-            finally:
-                # 임시파일 삭제
-                try:
-                    os.unlink(tmp_path)
-                except Exception:
-                    pass
+            return await self._service.infer(file)
 
 
-        #
-        #
-        # @app.get("/download")
-        # def health_download():
-        #     from Config.ConfigHelper import ConfigHelper
-        #     path = ConfigHelper.GetCachePath()
-        #
-        #     print(f"path:{path}")
-        #
-        #     from Inference.InferenceManager import InferenceManager
-        #     InferenceManager.instance().DownloadModel()
-        #
-        #     return {"status": "ok"}
-
-
+        from Models.RestModel import InferResponse
+        @app.post("/infer_vis" )
+        async def infer_vis(request: Request,file: UploadFile = File(...)):
+            return await self._service.infer_vis(file)
 
     pass
